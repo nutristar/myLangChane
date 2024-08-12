09e883808801f5506bca0648b6250b5c396ee302
nutristar <deniszmail@gmail.com>
2024-08-13 01:55:01 +0200

    sknd_commit

---
+++ gptchain.py
@@ -0,0 +1,24 @@
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
+import click
+
+@click.command()
+@click.option("--query", "-q", required=True)
+def retrieve(query):
+    loader = TextLoader("data.txt")
+    index = VectorstoreIndexCreator().from_loaders([loader])
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model="gpt-3.5-turbo"),
+        retriever=index.vectorstore.as_retriever(
+            search_kwargs={"k" : 1}
+        )
+    )
+    chat_log = []
+    result = chain({"question": query, "chat_hisstory": chat_log})
+    click.echo(result["answer"])
+    chat_log.append((query,result["answer"]))
+
+if __name__ == "__main__":
+    retrieve()
